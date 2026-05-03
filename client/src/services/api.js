3bc4a63a7e28ee1e1579d37cff66273c1a238c40
jeevanshetwe000 <jeevanshetwe000@gmail.com>
2026-05-03 16:30:13 +0530

    edit prod level api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -134,7 +134,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/useAuthStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL  + "/api" || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
